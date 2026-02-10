1a49cc4bd44504cd63e1ed6f3d8112a088608497
shahurajmane <maneshahuraj007@gmail.com>
2026-02-10 11:23:43 +0530

    Create Exp2Act3.py

---
+++ Exp2/Exp2Act3.py
@@ -0,0 +1,12 @@
+# Chech Driver is Overspeeding
+"""
+Created on Tue Feb 10 10:35:50 2026
+
+@author: Shahuraj Mane
+"""
+
+speed =  int(input("Enter speed km/h:"))
+if speed>60:
+    print("Overspeeding!Fine Applied.")
+else:
+    print("Within Limit.")
