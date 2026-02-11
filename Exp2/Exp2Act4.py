0369250a49f3810e41c28bdb316a31336198d24f
shahurajmane <maneshahuraj007@gmail.com>
2026-02-11 22:31:57 +0530

    Create Exp2Act4.py

---
+++ Exp2/Exp2Act4.py
@@ -0,0 +1,13 @@
+# Check Eligibility
+"""
+Created on Wed Feb 11 22:03:46 2026
+
+@author: Shahuraj Mane
+"""
+
+age = int(input("Enter age:"))
+    
+if age >= 18:
+    print("You are eligible to watch this movie.")
+else:
+    print("You are not eligible to watch this movie because you are under 18.")
