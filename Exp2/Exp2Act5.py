9823b1bdd6986d1af7edd61cf7b1fa2c0e32d258
shahurajmane <maneshahuraj007@gmail.com>
2026-02-12 23:04:12 +0530

    Add loan approval check based on salary input

---
+++ Exp2/Exp2Act5.py
@@ -0,0 +1,12 @@
+# Check Loan approval
+"""
+Created on Thu Feb 12 20:45:38 2026
+
+@author: Shahuraj Mane
+"""
+
+salary = int(input("Enter your per month salary: "))
+if salary >= 25000:
+    print("Approved")
+else:
+    print("Rejcted")
