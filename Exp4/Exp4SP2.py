c245a14de1b736100042e563791bd65f39b3719c
shahurajmane <maneshahuraj007@gmail.com>
2026-03-24 10:17:57 +0530

    Create Exp4SP2.py

---
+++ Exp4/Exp4SP2.py
@@ -0,0 +1,10 @@
+# -*- reverse a given string using slicing -*-
+"""
+Created on Tue Mar 24 10:12:24 2026
+
+@author: Shahuraj Mane
+"""
+
+string = input("Enter a string: ")
+reversed_string = string[::-1]
+print("Reversed string:", reversed_string)
