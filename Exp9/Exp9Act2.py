3c1508ea56a077313c4bd9f5b497a6b585443862
shahurajmane <maneshahuraj007@gmail.com>
2026-04-25 14:26:50 +0530

    Create Exp9Act2.py

---
+++ Exp9/Exp9Act2.py
@@ -0,0 +1,15 @@
+# -*- Display today's date in hospital appointment system -*-
+"""
+Created on Sat Apr 25 14:23:47 2026
+
+@author: Shahuraj
+"""
+
+from datetime import date
+
+# Get today's date
+today = date.today()
+
+# Display date
+print("Hospital Appointment System")
+print("Today's Date:", today)
