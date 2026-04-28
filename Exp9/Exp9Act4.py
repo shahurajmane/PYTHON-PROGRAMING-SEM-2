adec7551a66b6b31afa145d53d95299412c3b3fe
shahurajmane <maneshahuraj007@gmail.com>
2026-04-28 10:41:45 +0530

    Create Exp9Act4.py

---
+++ Exp9/Exp9Act4.py
@@ -0,0 +1,12 @@
+# -*- Generate OTP using random module -*-
+"""
+Created on Tue Apr 28 05:07:44 2026
+
+@author: Shahuraj
+"""
+
+import random
+
+otp = random.randint(1000, 9999)
+
+print("Your OTP is:", otp)
